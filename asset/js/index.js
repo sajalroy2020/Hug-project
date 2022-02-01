78c82504cc4fa562506f0d63149d6532ce68aa40
sajalroy2020 <sajal2020roy@gmail.com>
2022-02-01 18:41:38 +0600

    hrllo

---
+++ asset/js/index.js
@@ -0,0 +1,7 @@
+
+let mainMenu = document.getElementById('main_menu');
+let mainBtn = document.getElementById('main_btn');
+
+mainBtn.addEventListener('click', function() {
+   mainMenu.classList.toggle('d-none');
+});
